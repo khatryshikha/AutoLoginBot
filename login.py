a2e5bb851d5eea37c94597fd8fefd753c905cdfc
khatryshikha <shikhakhatry2612@gmail.com>
2020-02-23 17:38:45 +0530

    new changes

--- login.py
+++ login.py
@@ -11,23 +11,30 @@ def job():
 
     driver = webdriver.Chrome(executable_path='/home/shikha/chromedriver')
     driver.get(('http://172.22.252.11/login'))
+    
+    try:
+        if driver.current_url == 'http://172.22.252.11/status':
+            driver.quit()
+        username = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.NAME, "username")))
+        username.send_keys(usernameStr)
 
-    username = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.NAME, "username")))
-    username.send_keys(usernameStr)
+        password = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.NAME, "password")))
+        password.send_keys(passwordStr)
 
-    password = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.NAME, "password")))
-    password.send_keys(passwordStr)
+        signInButton = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr/td/table/tbody/tr[1]/td/form/table/tbody/tr[3]/td[2]/input")))
+        signInButton.click()
+        if driver.current_url == 'http://172.22.252.11/status':
+            driver.quit()
+    except expression as identifier:
+        pass
 
-    signInButton = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr/td/table/tbody/tr[1]/td/form/table/tbody/tr[3]/td[2]/input")))
-    signInButton.click()
-    driver.quit()
 
-schedule.every().day.at("00:00").do(job)
 schedule.every().day.at("08:00").do(job)
-schedule.every().day.at("16:00").do(job)
+schedule.every().day.at("18:00").do(job)
+schedule.every().day.at("00:00").do(job)
 
 while True:
     schedule.run_all()
